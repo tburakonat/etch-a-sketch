e786a99f930f3557bbbf4c72d4fb8b18cfec3d74
tburakonat <tburakonat@gmail.com>
2022-05-13 18:10:27 +0200

    added function to create custom divs

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function createGrid() {
-    const userInput = prompt("How many columns do you want")
+function createGrid(columns) {
+    const userInput = columns || prompt("How many columns do you want")
+    const gridContainer = document.getElementById("grid-container")
+
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+
     for (let i = 0; i < (userInput * userInput); i++) {
-        const gridContainer = document.getElementById("grid-container")
         const div = document.createElement("div")
         gridContainer.style.gridTemplateColumns = `repeat(${userInput}, 15px)`
         gridContainer.append(div)
     }
 }
+
+window.addEventListener("load", () => {
+    createGrid(16)
+})
+
+const gridButton = document.getElementById("create-grid")
+
+gridButton.addEventListener("click", () => {
+    createGrid()
+})
